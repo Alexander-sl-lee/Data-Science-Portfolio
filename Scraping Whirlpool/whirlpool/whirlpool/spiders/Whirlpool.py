604938312bd036f052684671fd796887cb39428e
Phobic <Alexander.lee@live.com.au>
2018-02-15 23:18:18 +1100

    whirlpool scrapy

---
+++ Scraping Whirlpool/whirlpool/whirlpool/spiders/Whirlpool.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import numpy as np
+import scipy.stats as stats
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import requests
+import pprint
+
+from bs4 import BeautifulSoup
+
+class WhirlpoolSpider(scrapy.Spider):
+    name = 'Whirlpool'
+    allowed_domains = ['http://forums.whirlpool.net.au']
+    start_urls = ['http://forums.whirlpool.net.au/forum-replies.cfm?t=2691997']
+
+    def parse(self, response):
+
+        item = {}
+        
+        self.log('------------------------------------')
+        self.log('I just visited: ' + response.url)
+        self.log('------------------------------------')
+           
+        r = requests.get('http://forums.whirlpool.net.au/forum-replies.cfm?t=2691997')
+        soup = BeautifulSoup(r.text, "lxml")
+
+        test = soup.find_all("div", class_="replytext bodytext")
+
+        for i in test:
+            item[""]
+
+        yield item
+
